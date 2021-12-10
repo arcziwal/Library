97d1642845150b4446de79b6d7c7d4e519c33daa
Artur Waligora <waligoraartur95@gmail.com>
2021-12-10 20:34:22 +0100

    added site to generate table with all books

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from models import Author
+from models import Author, Book
 from psycopg2 import connect, OperationalError
 
 USER = "postgres"
@@ -11,17 +11,43 @@ app = Flask(__name__)
 
 
 @app.route('/add_book', methods=['GET', 'POST'])
-def main():
+def add_book_to_db():
+    cnx = connect(database=DATABASE, user=USER, password=PASSWORD, host=HOST)
+    cursor = cnx.cursor()
+    cnx.autocommit = True
     if request.method == "GET":
-        cnx = connect(database=DATABASE, user=USER, password=PASSWORD, host=HOST)
-        cursor = cnx.cursor()
         authors = Author.load_all_authors(cursor)
         cursor.close()
         cnx.close()
         authors_data = []
         for author in authors:
-            authors_data.append(f"{author.last_name} {author.first_name}")
+            authors_data.append(f"{author.full_name}")
         return render_template("add_book_form.html", authors=authors_data)
+    elif request.method == "POST":
+        book_title = request.form["book_title"]
+        book_isbn = request.form["isbn"]
+        book_author = request.form["author"]
+        book_description = request.form["description"]
+        author_id = Author.load_author_by_full_name(cursor, book_author)
+        book = Book(book_title, book_isbn, author_id, book_description)
+        book.save_to_db(cursor)
+        cursor.close()
+        cnx.close()
+        return f"Książka pod tytułem {book_title} została pomyślnie dodana do bazy danych"
+
+
+@app.route('/books', methods=['GET'])
+def show_all_books():
+    cnx = connect(database=DATABASE, user=USER, password=PASSWORD, host=HOST)
+    cursor = cnx.cursor()
+    cnx.autocommit = True
+    books = Book.load_all_books(cursor)
+    data_to_show = []
+    for book in books:
+        author_name = Author.load_author_by_id(cursor, book.author_id)
+        data_to_show.append((book.title, author_name, book.isbn))
+    print(data_to_show)
+    return render_template("book_list.html", books=data_to_show)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -3,6 +3,7 @@ class Author:
         self._id = -1
         self.first_name = first_name
         self.last_name = last_name
+        self.full_name = f"{self.last_name} {self.first_name}"
 
     @property
     def id(self):
@@ -10,38 +11,55 @@ class Author:
 
     def save_to_db(self, cursor):
         if self._id == -1:
-            sql = "INSERT INTO authors(first_name, last_name) VALUES (%s, %s) RETURNING id"
-            values = (self.first_name, self.last_name)
+
+            sql = "INSERT INTO authors(first_name, last_name, full_name) VALUES (%s, %s, %s) RETURNING id"
+            values = (self.first_name, self.last_name, self.full_name)
             cursor.execute(sql, values)
             self._id = cursor.fetchone()[0]
             return True
         else:
-            sql = "UPDATE authors SET first_name=%s, last_name=%s WHERE id=%s"
-            values = (self.first_name, self.last_name, self._id)
+            sql = "UPDATE authors SET first_name=%s, last_name=%s, full_name=%s WHERE id=%s"
+            values = (self.first_name, self.last_name, self.full_name, self._id)
             cursor.execute(sql, values)
             return True
 
     @staticmethod
     def load_all_authors(cursor):
-        sql = "SELECT id, first_name, last_name FROM authors"
+        sql = "SELECT id, first_name, last_name, full_name FROM authors"
         authors = []
         cursor.execute(sql)
         for row in cursor.fetchall():
-            id_, first_name, last_name = row
+            id_, first_name, last_name, full_name = row
             author = Author()
             author._id = id_
             author.first_name = first_name
             author.last_name = last_name
+            author.full_name = full_name
             authors.append(author)
         return authors
 
+    @staticmethod
+    def load_author_by_full_name(cursor, author_full_name):
+        sql = f"SELECT id FROM authors WHERE full_name=%s"
+        value = str(author_full_name)
+        print(value)
+        cursor.execute(sql, (value,))
+        author_id = cursor.fetchone()[0]
+        return author_id
+
+    @staticmethod
+    def load_author_by_id(cursor, author_id):
+        sql = f"SELECT full_name FROM authors WHERE id=%s"
+        cursor.execute(sql, (author_id,))
+        author_full_name = cursor.fetchone()[0]
+        return author_full_name
 
 class Book:
-    def __init__(self, author_id, title="", isbn="", description=""):
+    def __init__(self, title="", isbn="", author_id=-1, description=""):
         self._id = -1
-        self.author_id = author_id
         self.title = title
         self.isbn = isbn
+        self.author_id = author_id
         self.description = description
 
     @property
@@ -50,7 +68,7 @@ class Book:
 
     def save_to_db(self, cursor):
         if self._id == -1:
-            sql = "INSERT INTO books(title, isbn, author_id, desription) VALUES (%s, %s, %s, %s) RETURNING id"
+            sql = "INSERT INTO books(title, isbn, author_id, description) VALUES (%s, %s, %s, %s) RETURNING id"
             values = (self.title, self.isbn, self.author_id, self.description)
             cursor.execute(sql, values)
             self._id = cursor.fetchone()[0]
@@ -60,3 +78,20 @@ class Book:
             values = (self.title, self.isbn, self.author_id, self.description, self._id)
             cursor.execute(sql, values)
             return True
+
+    @staticmethod
+    def load_all_books(cursor):
+        sql = "SELECT id, title, isbn, author_id, description FROM books"
+        books = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            id_, title, isbn, author_id, description = row
+            book = Book()
+            book._id = id_
+            book.title = title
+            book.isbn = isbn
+            book.author_id = author_id
+            book.description = description
+            books.append(book)
+        return books
+
